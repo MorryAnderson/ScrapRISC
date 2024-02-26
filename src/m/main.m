1d84d402fb0a45b91f807e8e275b466945dd0594
Morry4C <morry4c@qq.com>
2024-02-27 05:21:36 +0800

    Add files via upload

--- src/m/main.m
+++ src/m/main.m
@@ -1,8 +1,23 @@
-top_name = "M3_top";
+%% 文件路径
+bp_folder_path = "../../outputs/temp";
 
-net_ext = ".net";
-net_dir = "../../outputs/temp/";
-net_path =  net_dir + top_name + net_ext;
-out_dir = "../../outputs/temp";
+iromfile = "../../_Logisim/ROM/debug.irom";
+dromfile = "../../_Logisim/ROM/gol_Glider.drom";
 
-Net2Blueprint(net_path, out_dir);
+netfile_irom = "../../outputs/temp/M3_instr_rom.net";
+netfile_drom = "../../outputs/temp/M3_data_rom.net";
+
+
+%% 参数
+DATA_WIDTH = 32;
+ROM_DEPTH_INSTR = 128;
+ROM_DEPTH_DATA = 64;
+
+
+%% Instr ROM
+GenNetRomPlane(iromfile,netfile_irom, ROM_DEPTH_INSTR);
+Net2Blueprint(netfile_irom, bp_folder_path);
+
+%% Data ROM
+GenNetRomPlane(dromfile,netfile_drom, ROM_DEPTH_DATA);
+Net2Blueprint(netfile_drom, bp_folder_path);
